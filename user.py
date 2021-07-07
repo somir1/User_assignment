f901abe3c46f3048e49535978c167e89d865631b
somir1 <leonsomir@gmail.com>
2021-07-07 14:17:46 -0500

    chaining the methods

--- user.py
+++ user.py
@@ -8,34 +8,22 @@ class user:
 
     def make_Deposit(self, amount):
         self.amount += amount
+        return self
 
     def make_withdrawal(self, amount):
         self.amount -= amount
+        return self
 
     def display_user_balance(self):
         print(f"Your new balance is: {self.amount}" )
+
 
 user1 = user('Samir','Hossain', 100000)
 user2 = user('Jim','Carrey', 950000)
 user3 = user('Mike','Stone', 5000)
 
-user1.make_Deposit(50)
-user1.make_Deposit(100)
-user1.make_Deposit(5)
-user1.make_withdrawal(10)
-user1.display_user_balance()
-print(user1.amount)
+user1.make_Deposit(50).make_Deposit(100).make_Deposit(5).make_withdrawal(10).display_user_balance()
 
-user2.make_Deposit(50)
-user2.make_Deposit(10)
-user2.make_withdrawal(10)
-user2.make_withdrawal(5)
-user2.display_user_balance()
-print(user2.amount)
+user2.make_Deposit(50).make_Deposit(10).make_withdrawal(10).make_withdrawal(5).display_user_balance()
 
-user3.make_Deposit(500)
-user3.make_withdrawal(10)
-user3.make_withdrawal(5)
-user3.make_withdrawal(595)
-user3.display_user_balance()
-print(user3.amount)
+user3.make_Deposit(500).make_withdrawal(10).make_withdrawal(5).make_withdrawal(595).display_user_balance()
